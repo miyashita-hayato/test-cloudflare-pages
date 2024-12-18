81bce7a9126f9a4a1fd4d383bd0ef4786afa9d96
miyashita-hayato <miyashita-hayato@mitsue.co.jp>
2024-12-18 21:03:32 +0900

    feat: 環境ごとにAPIを切り替えるサンプルページを追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,24 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+const branch = process.env.CF_PAGES_BRANCH || 'unknown';
+
+const apiUrl = (() => {
+    switch (branch) {
+        case 'main':
+            return 'https://jsonplaceholder.typicode.com/posts/1';
+        case 'staging':
+            return 'https://jsonplaceholder.typicode.com/posts/2';
+        default:
+            return 'https://jsonplaceholder.typicode.com/posts/3';
+    }
+})();
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    vite: {
+        define: {
+            'process.env.PUBLIC_API_URL': JSON.stringify(apiUrl),
+        }
+    }
+});
